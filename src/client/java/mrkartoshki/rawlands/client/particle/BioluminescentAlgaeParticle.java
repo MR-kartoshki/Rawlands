370d35970f16994793d823433d4f01565174dfe9
Nikita <129411149+MR-kartoshki@users.noreply.github.com>
2026-05-17 02:42:46 +0200

    Fix Algae particle and change biome generation a bit

--- src/client/java/mrkartoshki/rawlands/client/particle/BioluminescentAlgaeParticle.java
+++ src/client/java/mrkartoshki/rawlands/client/particle/BioluminescentAlgaeParticle.java
@@ -5,17 +5,19 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleProvider;
-import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.SingleQuadParticle;
 import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.util.LightCoordsUtil;
 import net.minecraft.util.RandomSource;
 
 @Environment(EnvType.CLIENT)
-public class BioluminescentAlgaeParticle extends Particle {
+public class BioluminescentAlgaeParticle extends SingleQuadParticle {
 
     protected BioluminescentAlgaeParticle(ClientLevel level, double x, double y, double z,
-                                           RandomSource random) {
-        super(level, x, y, z);
+                                           TextureAtlasSprite sprite, RandomSource random) {
+        super(level, x, y, z, sprite);
         this.lifetime = 60 + random.nextInt(60);
         this.xd = (random.nextDouble() - 0.5) * 0.008;
         this.yd = 0.008 + random.nextDouble() * 0.008;
@@ -23,20 +25,28 @@ public class BioluminescentAlgaeParticle extends Particle {
     }
 
     @Override
-    public ParticleRenderType getGroup() {
-        return ParticleRenderType.SINGLE_QUADS;
+    protected SingleQuadParticle.Layer getLayer() {
+        return SingleQuadParticle.Layer.TRANSLUCENT;
+    }
+
+    @Override
+    public int getLightCoords(float partialTick) {
+        return LightCoordsUtil.withBlock(super.getLightCoords(partialTick), 15);
     }
 
     @Environment(EnvType.CLIENT)
     public static class Factory implements ParticleProvider<SimpleParticleType> {
+        private final SpriteSet sprites;
+
         public Factory(SpriteSet sprites) {
+            this.sprites = sprites;
         }
 
         @Override
         public Particle createParticle(SimpleParticleType type, ClientLevel level,
                                         double x, double y, double z,
                                         double vx, double vy, double vz, RandomSource random) {
-            return new BioluminescentAlgaeParticle(level, x, y, z, random);
+            return new BioluminescentAlgaeParticle(level, x, y, z, sprites.first(), random);
         }
     }
 }
